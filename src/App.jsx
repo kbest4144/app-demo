b12c2406151d5208f304a284317c43a8c8ddb14b
collins <you@example.com>
2025-03-04 18:29:16 +0300

    changes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='container'>
+      hello my name is collins mbuthia help me out
+      i am the best
+    </div>
+  )
+}
+
+export default App
